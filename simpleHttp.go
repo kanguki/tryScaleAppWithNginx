59c1588d9191e999633c1d5ae20c49a51b1bf6fc
Mo Nguyen <mo.nguyen@difisoft.com>
2022-04-01 16:17:50 +0700

    simulate case backend is behind multiple proxies

--- simpleHttp.go
+++ simpleHttp.go
@@ -9,17 +9,32 @@ import (
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	var name, _ = os.Hostname()
-    fmt.Fprintf(w, "hello from %v\n", name)
+	ip := ReadUserIP(req)
+	fmt.Printf("ip: %v\n", ip)
+	fmt.Fprintf(w, "hello from %v\n", name)
+}
+
+func ReadUserIP(r *http.Request) string {
+	fmt.Println("headers:", r.Header)
+	fmt.Println("X-Real_Ip", r.Header.Get("X-Real-Ip"), "X-Forwarded-For", r.Header.Get("X-Forwarded-For"), "RemoteAddr", r.RemoteAddr)
+	IPAddress := r.Header.Get("X-Real-Ip")
+	if IPAddress == "" {
+		IPAddress = r.Header.Get("X-Forwarded-For")
+	}
+	if IPAddress == "" {
+		IPAddress = r.RemoteAddr
+	}
+	return IPAddress
 }
 
 func main() {
-    http.HandleFunc("/", hello)
+	http.HandleFunc("/", hello)
 
-    port := os.Args[1]
-    if _, err := strconv.Atoi(port); err != nil {
-	    fmt.Printf("Expected first arg to be a port number but receive %v\n", port)
-	    os.Exit(1)
-    }
-    fmt.Printf("Heol~, u have a http server running on port %v", port)
-    http.ListenAndServe(":" + port, nil)
+	port := os.Args[1]
+	if _, err := strconv.Atoi(port); err != nil {
+		fmt.Printf("Expected first arg to be a port number but receive %v\n", port)
+		os.Exit(1)
+	}
+	fmt.Printf("Heol~, u have a http server running on port %v\n", port)
+	http.ListenAndServe(":"+port, nil)
 }
